0f9a07677bfc1faaa8db11ebb1a249a49b690152
ihyaulumuddin044 <ihya400116@gmail.com>
2024-09-09 19:32:05 +0700

    Footer Done

--- src/layout/Main.jsx
+++ src/layout/Main.jsx
@@ -3,13 +3,14 @@ import Navbar from '../component/Navbar'
 import Benners from '../component/Benners'
 import '../App.css'
 import Home from '../pages/home/Home'
+import Footer from '../component/Footer'
 
 const Main = () => {
   return (
     <div>
         <Navbar />
         <Home />
-        main
+        <Footer />
 
     </div>
   )
