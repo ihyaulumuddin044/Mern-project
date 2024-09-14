d2b0f2aab1999552a16fd173095cba12d3b901a8
ihyaulumuddin044 <ihya400116@gmail.com>
2024-09-14 13:08:49 +0700

    modal component  done

--- src/component/Modal.jsx
+++ src/component/Modal.jsx
@@ -1,15 +1,27 @@
 import React from "react";
 import { FaFacebookF, FaGithub, FaGoogle } from "react-icons/fa";
 import { Link } from "react-router-dom";
+import {useForm} from "react-hook-form"
+
 
 const Modal = () => {
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm();
+
+  const onSubmit = (data) => console.log(data);
+  
   return (
     <div>
       <dialog id="my_modal_5" className="modal modal-middle sm:modal-middle">
         <div className="modal-box">
           <h3 className="font-bold text-lg">Login Please!</h3>
+          
+          {/* email */}
           <div className="modal-action flex flex-col justify-center mt-0">
-            <form className="card-body" method="dialog">
+            <form onSubmit={handleSubmit(onSubmit)} className="card-body" method="dialog">
               <div className="form-control">
                 <label className="label">
                   <span className="label-text">Email</span>
@@ -18,9 +30,11 @@ const Modal = () => {
                   type="email"
                   placeholder="email"
                   className="input input-bordered"
-                  required
+                  {...register("email")}
                 />
               </div>
+
+              {/* password */}
               <div className="form-control">
                 <label className="label">
                   <span className="label-text">Password</span>
@@ -29,7 +43,7 @@ const Modal = () => {
                   type="password"
                   placeholder="password"
                   className="input input-bordered"
-                  required
+                  {...register("password")}
                 />
                 <label className="label mt-1">
                   <a href="#" className="label-text-alt link link-hover">
