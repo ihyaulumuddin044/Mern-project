655707d647fa594cdad19de354ad7f7f1a7de227
ihyaulumuddin044 <ihya400116@gmail.com>
2024-09-26 13:50:32 +0700

    post ke data base ketika

--- src/component/Cards.jsx
+++ src/component/Cards.jsx
@@ -1,39 +1,59 @@
 import React, { useContext, useState } from "react";
 import { Link } from "react-router-dom";
-import {FaHeart} from 'react-icons/fa'
+import { FaHeart } from "react-icons/fa";
 import { AuntContext } from "../context/AuntProvider";
 const Cards = ({ item }) => {
-  const {name, image, price, recipe, _id } = item
-    const[isHeartFilltered, setIsHeartFilled] = useState(false);
-    const {user} = useContext(AuntContext)
-    // console.log(user)
-    // handleAddToCart
-    const handleAddToCart = (item) => {
-        // console.log("added to cart", item);
-        if (user && user?.email) {
-            // add to cart
-            const cartItem = {menuItenId: _id, name, quantity: 1,image, price, email: user.email};
-            console.log(cartItem)
-        } else {
-          
-        }
+  const { name, image, price, recipe, _id } = item;
+  const [isHeartFilltered, setIsHeartFilled] = useState(false);
+  const { user } = useContext(AuntContext);
+  // console.log(user)
+  // handleAddToCart
+  const handleAddToCart = (item) => {
+    // console.log("added to cart", item);
+    if (user && user?.email) {
+      // add to cart push data to serve
+      const cartItem = {
+        menuItenId: _id,
+        name,
+        quantity: 1,
+        image,
+        price,
+        email: user.email,
+      };
+      // console.log(cartItem)
+      fetch("http://localhost:6001/cards", {
+        method: "POST",
+        headers: {
+          'content-type': 'application/json'
+        },
+        body: JSON.stringify(cartItem),
+      })
+        .then((res) => res.json())
+        .then((data) => console.log(data));
+    } else {
     }
+  };
 
-    const handleHeartClick = () => {
-        setIsHeartFilled(!isHeartFilltered);
-    }
+  const handleHeartClick = () => {
+    setIsHeartFilled(!isHeartFilltered);
+  };
   return (
-    <div  className="card bg-base-100 w-96 shadow-xl relative">
-        <div className={`rating gap-1 absolute right-2 top-2 p-4 heartStar bg-green ${isHeartFilltered ? "text-rose-500" : "text-white"}`}
+    <div className="card bg-base-100 w-96 shadow-xl relative">
+      <div
+        className={`rating gap-1 absolute right-2 top-2 p-4 heartStar bg-green ${
+          isHeartFilltered ? "text-rose-500" : "text-white"
+        }`}
         onClick={handleHeartClick}
-        >
-            <FaHeart className="h-5 w5 cursor-pointer"/>
-        </div>
+      >
+        <FaHeart className="h-5 w5 cursor-pointer" />
+      </div>
       <Link key={item._id} to={`/menu/${item._id}`}>
         <figure>
-          <img src={item.image} 
-          alt=""
-          className="hover:scale-105 transition-all duration-200 md:h-72" />
+          <img
+            src={item.image}
+            alt=""
+            className="hover:scale-105 transition-all duration-200 md:h-72"
+          />
         </figure>
       </Link>
       <div className="card-body">
@@ -44,7 +64,12 @@ const Cards = ({ item }) => {
           <h5 className="font-semibold">
             <span className="text-sm text-red">${""}</span> {item.price}
           </h5>
-          <button className="btn bg-green text-white" onClick={() => handleAddToCart(item)}>Buy Now</button>
+          <button
+            className="btn bg-green text-white"
+            onClick={() => handleAddToCart(item)}
+          >
+            Buy Now
+          </button>
         </div>
       </div>
     </div>
