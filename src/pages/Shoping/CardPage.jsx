7c9558db0812148a448de13f9efac275e4651df1
ihyaulumuddin044 <ihya400116@gmail.com>
2024-10-02 15:31:42 +0700

    cardpage shope update

--- src/pages/Shoping/CardPage.jsx
+++ src/pages/Shoping/CardPage.jsx
@@ -1,8 +1,29 @@
 import React from "react";
 import useCart from "../../hooks/useCart";
 import { FaTrash } from "react-icons/fa"
+import Swal from "sweetalert2";
+
+
 const CardPage = () => {
   const [card, refetch] = useCart();
+
+  const handleDelete = (item) => {
+    Swal.fire({
+      title: "Are you sure?",
+      text: "You won't be able to revert this!",
+      icon: "warning",
+      showCancelButton: true,
+      confirmButtonColor: "#3085d6",
+      cancelButtonColor: "#d33",
+      confirmButtonText: "Yes, delete it!"
+    }).then((result) => {
+      if (result.isConfirmed) {
+        fetch()
+      }
+    });
+  }
+
+
   return (
     <div className="section-container">
       {/* benner */}
@@ -61,7 +82,7 @@ const CardPage = () => {
                 <td>{item.quantity}</td>
                 <td>{item.price}</td>
                 <th>
-                  <button className="btn btn-ghost btn-xs">
+                  <button className="btn btn-ghost text-red btn-xs" onClick={() => handleDelete(item)}>
                     <FaTrash />
                   </button>
                 </th>
