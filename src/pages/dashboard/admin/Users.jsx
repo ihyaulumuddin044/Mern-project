15fc654eb5c969a44b1f2a2ecbe8ff5175a2a5cb
ihyaulumuddin044 <ihya400116@gmail.com>
2024-10-15 15:15:46 +0700

    update admin dasboard

--- src/pages/dashboard/admin/Users.jsx
+++ src/pages/dashboard/admin/Users.jsx
@@ -1,8 +1,26 @@
+import {useQuery} from '@tanstack/react-query'
 import React from 'react'
+// import User from '../../../../../Foodis-server/api/models/User'
+// import axios from 'axios'
 
 const Users = () => {
+  // const axiosSecure = useAxiosSecure()
+  const {refact, data: users = []} = useQuery({
+    queryKey: ['user'],
+    queryFn: async () => {
+      const res = await fetch(`http://localhost:6001/user`)
+      if (!res.ok) {
+        throw new Error('Network response was not ok');}
+      console.log()
+      // const data = await res.json();
+      // console.log('Data fetched from API:', data); // Tambahkan ini untuk memeriksa respons
+      // return data;
+      return res.json()
+    }
+  })
+  console.log(users)
   return (
-    <div>Users</div>
+    <div>Users</div>  
   )
 }
 
