ce87e581df1621ad11689e379e4bc7af736a059a
ihyaulumuddin044 <ihya400116@gmail.com>
2024-10-16 13:06:29 +0700

    update user admin page

--- src/pages/dashboard/admin/Users.jsx
+++ src/pages/dashboard/admin/Users.jsx
@@ -1,5 +1,6 @@
 import { useQuery } from "@tanstack/react-query";
 import React from "react";
+import { FaTrashAlt, FaUser, FaUsers } from "react-icons/fa"
 // import User from '../../../../../Foodis-server/api/models/User'
 // import axios from 'axios'
 
@@ -22,9 +23,44 @@ const Users = () => {
   console.log(users);
   return (
     <div>
-      <div>
+      <div className="flex justify-between items-center m-4">
         <h5>all users</h5>
         <h5>total user: {users.length}</h5>
+      </div>
+      {/* table */}
+      <div>
+        <div className="overflow-x-auto">
+          <table className="table table-zebra md:w-[870px]">
+            {/* head */}
+            <thead className="bg-green font-bold text-white">
+              <tr>
+                <th>#</th>
+                <th>Name</th>
+                <th>Email</th>
+                <th>Role</th>
+                <th>Action</th>
+              </tr>
+            </thead>
+            <tbody>
+              {/* row 1 */}
+              {users.map((user, index) => (
+                <tr key={user._id}>
+                  <th>{index + 1}</th>
+                  <td>{user.name}</td>
+                  <td>{user.email}</td>
+                  <td>{
+                    user.role === "admin" ? "admin" : (
+                      <button className="btn btn-circle btn-xs bg-orange-500 text-white"><FaUsers /></button>
+                    )
+                    }</td>
+                  <td>
+                    <button className="btn btn-ghost btn-xs bg-red"><FaTrashAlt className="text-white"/> </button>
+                  </td>
+                </tr>
+              ))}
+            </tbody>
+          </table>
+        </div>
       </div>
     </div>
   );
