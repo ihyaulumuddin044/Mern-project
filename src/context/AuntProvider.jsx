647efb4d2bffbb1fc0edd9247aad75e434c71acc
ihyaulumuddin044 <ihya400116@gmail.com>
2024-10-18 13:24:06 +0700

    modify context page

--- src/context/AuntProvider.jsx
+++ src/context/AuntProvider.jsx
@@ -10,6 +10,8 @@ import {
   updateProfile,
 } from "firebase/auth";
 import app from "../firebase/firebase.config";
+import axios, { Axios } from "axios";
+import { errorPrefix } from "@firebase/util";
 
 export const AuntContext = createContext();
 const AuntProvider = ({ children }) => {
@@ -20,7 +22,7 @@ const AuntProvider = ({ children }) => {
 
   // create and accont
   const createUser = (email, password) => {
-    setLoading(true)
+    setLoading(true);
     return createUserWithEmailAndPassword(auth, email, password);
     // .then((userCredential) => {
     //   // Signed up
@@ -36,41 +38,53 @@ const AuntProvider = ({ children }) => {
 
   // signup with Gmail
   const sighUpWithGmail = () => {
-    setLoading(true)
+    setLoading(true);
     return signInWithPopup(auth, googleProvider);
   };
 
   // login using email and password
   const login = (email, password) => {
-    
     return signInWithEmailAndPassword(auth, email, password);
   };
 
   // laogout
   const logout = () => {
-
     return signOut(auth);
   };
 
   // update profile
   const updateProfileUser = (name, photoURL) => {
-     return updateProfile(auth.currentUser, { 
-      displayName: name, photoURL: photoURL
+    return updateProfile(auth.currentUser, {
+      displayName: name,
+      photoURL: photoURL,
     });
   };
 
   // check sign-in user
   useEffect(() => {
-      const unsubscribe = onAuthStateChanged(auth, currentUser =>{
-          // console.log(currentUser);
-          setUser(currentUser);
-          setLoading(false);
-      });
-
-      return () =>{
-          return unsubscribe();
+    const unsubscribe = onAuthStateChanged(auth, (currentUser) => {
+      // console.log(currentUser);
+      setUser(currentUser);
+      // get and set token
+      if (currentUser) {
+        const userInfo = { email: currentUser.email };
+        axios.post("http://localhost:6001/jwt", userInfo).then((response) => {
+          // console.log(response.data);
+          if (response.data.token) {
+            localStorage.setItem("access-token", response.data.token);
+          }
+        });
+      } else {
+        console.log("user not found");
       }
-  }, [])
+
+      setLoading(false);
+    });
+
+    return () => {
+      return unsubscribe();
+    };
+  }, []);
   const authinfo = {
     user,
     createUser,
