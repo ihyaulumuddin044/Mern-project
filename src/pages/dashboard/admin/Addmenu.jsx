030a907b0b2be3753e42e20ea499c4a43d36a3e8
ihyaulumuddin044 <ihya400116@gmail.com>
2024-10-22 12:49:47 +0700

    update dashboard addMenu

--- src/pages/dashboard/admin/Addmenu.jsx
+++ src/pages/dashboard/admin/Addmenu.jsx
@@ -1,68 +1,80 @@
 import React from "react";
+import { FaUtensils } from "react-icons/fa";
 
 const Addmenu = () => {
   return (
     <div className="w-full md:w-[870px] px-4 mx-auto">
       <h2 className="text-2xl font-semibold my-4">
-        Upload A <span className="text-green"> New Menu </span>
+        Upload A New <span className="text-green">Menu Item</span>
       </h2>
 
       {/* form here */}
       <div>
         <form>
-          <div className="form-control w-full my-6">
-            <label className="form-control w-full ">
-              <div className="label">
-                <span className="label-text">Recepise Name*</span>
-              </div>
+          <div className="form-control w-full">
+            <label className="label">
+              <span className="label-text">Recipe Name*</span>
+            </label>
+            <input
+              type="text"
+              placeholder="Recipe Name"
+              className="input input-bordered w-full"
+            />
+          </div>
+
+          {/* 2nd row */}
+          <div className="flex items-center gap-4">
+            {/* categories */}
+            <div className="form-control w-full my-6">
+              <label className="label">
+                <span className="label-text">Category*</span>
+              </label>
+              <select className="select select-bordered" defaultValue="default">
+                <option disabled value="default">
+                  Select a category
+                </option>
+                <option value="salad">Salad</option>
+                <option value="pizza">Pizza</option>
+                <option value="soup">Soup</option>
+                <option value="dessert">dessert</option>
+                <option value="drinks">Drinks</option>
+                <option value="popular">Popular</option>
+              </select>
+            </div>
+
+            {/* prices */}
+            <div className="form-control w-full">
+              <label className="label">
+                <span className="label-text">Price*</span>
+              </label>
               <input
-                type="text"
-                placeholder="Recepise Name"
+                type="number"
+                placeholder="Price"
                 className="input input-bordered w-full"
               />
-            </label>
-          </div>
-        </form>
-
-        {/* 2nd row */}
-        <div>
-          {/* select item category */}
-          <label className="form-control w-full  ">
-            <div className="label">
-              <span className="label-text">choose category</span>
             </div>
-            <select className="select select-bordered">
-              <option disabled selected>
-                Select a category
-              </option>
-              <option value="salad">Salad</option>
-              <option value="pizza">Pizza</option>
-              <option value="soup">Soup</option>
-              <option value="dessert">Dessert</option>
-              <option value="drinks">Drinks</option>
-              <option value="popular">Popolar</option>
-            </select>
-          </label>
-
-          {/* price */}
-
-          <div>
-            <form>
-              <div className="form-control w-full my-6">
-                <label className="form-control w-full ">
-                  <div className="label">
-                    <span className="label-text">Recepise Name*</span>
-                  </div>
-                  <input
-                    type="text"
-                    placeholder="Recepise Name"
-                    className="input input-bordered w-full"
-                  />
-                </label>
-              </div>
-            </form>
           </div>
-        </div>
+
+          {/* 3rd row */}
+          <div className="form-control">
+            <label className="label">
+              <span className="label-text">Recipe Details</span>
+            </label>
+            <textarea
+              className="textarea textarea-bordered h-24"
+              placeholder="Tell the worlds about your recipe"
+            ></textarea>
+          </div>
+
+          {/* 4th row */}
+          <div className="form-control w-full my-6">
+            <input type="file" className="file-input w-full max-w-xs" />
+          </div>
+
+          <button className="btn bg-green text-white px-6">
+            Add Item <FaUtensils />
+          </button>
+        </form>
       </div>
     </div>
   );
